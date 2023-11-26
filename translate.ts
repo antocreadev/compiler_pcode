eb95d0135d506c299d13581515c56330003557db
Anthony Menghi <antocreadev@anthonys-air.home>
2023-11-26 03:26:45 +0100

    add: INT in pcode

--- translate.ts
+++ translate.ts
@@ -1,8 +1,12 @@
 interface Pcode {
-    op : string;
-    arg : string | null | undefined;
+    op: string;
+    arg: string | null | undefined;
 }
 
-const translatePcode = ()=>{
+const pcode: Array<Pcode> = [];
 
-}
+const translatePcode = (tablesym: any) => {
+    pcode.push({ op: "INT", arg: tablesym.length });
+
+    return pcode;
+};
