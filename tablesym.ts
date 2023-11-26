8a041f1ed1dc1f5ea6b42d81edc8d27fdf5de139
Anthony Menghi <antocreadev@anthonys-air.home>
2023-11-26 19:40:12 +0100

    add: type in tablesym.ts

--- tablesym.ts
+++ tablesym.ts
@@ -1,11 +1,18 @@
 // deno-lint-ignore-file no-explicit-any
-const tablesym = (body : Array<any>) : any=> {
-    let declarations : any = [];
 
+import { Stmt, VariableDeclaration, DoWhileLoop } from "./ast.ts";
+const isVariableDeclaration = (stmt: Stmt): stmt is VariableDeclaration =>
+  stmt.kind === "VariableDeclaration";
+
+const isDoWhileLoop = (stmt: Stmt): stmt is DoWhileLoop =>
+  stmt.kind === "DoWhileLoop";
+
+const tablesym = (body : Array<Stmt>)=> {
+    let declarations :string[]= [];
     for (const element of body) {
-        if (element.kind === "VariableDeclaration") {
+        if (isVariableDeclaration(element)) {
             declarations.push(element.identifier.symbol);
-        } else if (Array.isArray(element.body)) {
+        } else if (isDoWhileLoop(element)) {
             declarations = declarations.concat(tablesym(element.body));
         }
     }
