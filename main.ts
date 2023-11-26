6e7ab93bfb4fe9ecc7bdfed8e6d0b80bad66848a
Anthony Menghi <antocreadev@anthonys-air.home>
2023-11-26 03:09:27 +0100

    add: function tablesym

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 import Parser from "./parser.ts";
 import { tokenize } from "./lexer.ts";
+import tablesym from "./tablesym.ts";
 
 const parser = new Parser();
 
 const input = Deno.readTextFile("./test.src");
-console.log(tokenize(await input));
+// console.log(tokenize(await input));
 const ast = parser.produceAST(await input);
-console.log(JSON.stringify(ast, null, 3))
+// console.log(JSON.stringify(ast, null, 3))
+
+const allvar = tablesym(ast.body);
+console.log(allvar)
+
---
+++ tablesym.ts
@@ -0,0 +1,15 @@
+// deno-lint-ignore-file no-explicit-any
+const tablesym = (body : Array<any>) : any=> {
+    let declarations : any = [];
+
+    for (const element of body) {
+        if (element.kind === "VariableDeclaration") {
+            declarations.push(element.identifier.symbol);
+        } else if (Array.isArray(element.body)) {
+            declarations = declarations.concat(tablesym(element.body));
+        }
+    }
+    return declarations;
+}
+
+export default tablesym;
