08a11da2a1168964ec08049a49e9e45c9ceb9916
Anthony Menghi <antocreadev@Anthonys-MacBook-Air.local>
2023-11-29 03:15:37 +0100

    update: translatepcode, class, type, structure, etc

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import Parser from "./Parser.ts"
 import TableSymbole from "./TableSymbole.ts";
-import { translatePcode } from "./translate.ts";
+import TranslatePcode from "./TranslatePcode.ts";
 
 const input = Deno.readTextFile("./test.src");
 
@@ -8,5 +8,8 @@ const parser = new Parser();
 const ast = parser.produceAST(await input);
 
 const tablesym = new TableSymbole(ast.body);
-console.log(translatePcode(await tablesym.generateTableSymbole(ast.body), ast.body))
-
+const translatePcode = new TranslatePcode(tablesym.generateTableSymbole(ast.body), ast.body);
+console.log(console.log(JSON.stringify(ast, null, 4)));
+translatePcode.generate_pcode(ast.body);
+console.table(translatePcode.get_tablesym());
+console.table(translatePcode.get_pcode());
